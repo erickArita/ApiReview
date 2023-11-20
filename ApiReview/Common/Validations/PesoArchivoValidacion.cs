13bcf618ed1171e65eaaf9813469487d80cf6948
melvin <melvin016orellana@gmail.com>
2023-11-20 16:06:53 -0600

    eliminando validaciones que parecen ser innesesarias

--- ApiReview/Common/Validations/PesoArchivoValidacion.cs
+++ ApiReview/Common/Validations/PesoArchivoValidacion.cs
@@ -12,25 +12,14 @@ public class PesoArchivoValidacion: ValidationAttribute
     
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        if (value is null)
-        {
-            return ValidationResult.Success;
-        }
+       
         IFormFile formFile = value as IFormFile;
-
-        if (formFile is null)
-        {
-            return ValidationResult.Success;
-        }
-
+        
         if (formFile.Length > pesoMaximoEnMegaBytes * 1024 * 1024)
         {
             return new ValidationResult($"El peso de la imagen no debe ser mayor a {pesoMaximoEnMegaBytes}MB");
         }
-        {
-            
-        }
-
+      
         return ValidationResult.Success;
     }
 }
